ba803b82ad55cabf79d1539f7fd4f369f147e001
ScooredMars-Work <o.jaworski@pilvin.pl>
2025-03-29 20:37:24 +0100

    added apps to core and urls to prevent django error

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("text/", include("text_processor.urls", namespace="text_processor")),
+    path("pesel/", include("pesel_validator.urls", namespace="pesel_validator")),
 ]
